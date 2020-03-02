eccf54694ea5e8e920459822959f3e2add203d80
trtrtr6 <2450260012@qq.com>
2020-03-02 14:49:42 +0800

    feat：初步完善mock解析

--- routers/mock.js
+++ routers/mock.js
@@ -15,12 +15,27 @@ router.use(function (req, res, next) {
 
 //新增一个mock接口
 router.post('/add', async (req, res) => {
-  var body = req.body
-  var mock = new Mock({
-    name: body.name,
-    path: body.path,
-    type: body.type,
-    reg: body.reg
+  const body = req.body
+  const { name, path, type, reg } = body
+  if (!(name && path && type && reg)) {
+    responseData.data = '字段不能为空'
+    res.json(responseData);
+    return
+  }
+  const count = await Mock.count({
+    path,
+    type
+  })
+  if (count && count > 0) {
+    responseData.data = '已存在对应的接口，添加失败'
+    res.json(responseData);
+    return
+  }
+  const mock = new Mock({
+    name,
+    path,
+    type,
+    reg
   })
   const doc = await mock.save()
   responseData.data = doc
@@ -41,6 +56,37 @@ router.get('/list', async (req, res) => {
   responseData.data = {
     list
   }
+  res.json(responseData);
+})
+
+//查看mock接口详情
+router.get('/del/:id', async (req, res) => {
+  const id = req.params.id
+  await Mock.deleteOne({
+    _id: id
+  })
+  res.json(responseData);
+})
+
+//查看mock接口详情
+router.post('/update/:id', async (req, res) => {
+  const body = req.body
+  const id = req.params.id
+  const { name, path, type, reg } = body
+  if (!(name && path && type && reg)) {
+    responseData.data = '字段不能为空'
+    res.json(responseData);
+    return
+  }
+  const mock = {
+    name,
+    path,
+    type,
+    reg
+  };
+  await Mock.findByIdAndUpdate({
+    _id: id
+  }, mock)
   res.json(responseData);
 })
 
--- routers/mockApi.js
+++ routers/mockApi.js
@@ -1,8 +1,41 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express')
+const router = express.Router()
+const Mock = require('../models/Mock')
 const constants = require('../utils/constants')
+const Mockjs = require('mockjs')
 
-var responseData;
+const MockData = (obj) => {
+  if (typeof obj === 'object') {
+    Object.keys(obj).forEach((key) => {
+      const val = obj[key]
+      if (typeof val === 'object') {
+        if (val.mock) {
+          const cls = val.class
+          if (cls === 'array') {
+            const custom = val.custom
+            const reg = val.reg
+            let i = 0
+            const arr = []
+            while (i < reg) {
+              i++
+              arr.push(MockData({ ...custom }))
+            }
+            obj[key] = arr
+          } else if (cls === 'image') {
+            obj[key] = Mockjs.Random.image(...val.reg)
+          }
+        } else {
+          MockData(val)
+        }
+      } else {
+        obj[key] = Mockjs.mock(val)
+      }
+    })
+  }
+  return obj
+}
+
+let responseData;
 
 router.use(function (req, res, next) {
   responseData = Object.assign({}, constants.RES_INFO.success, { data: {} })
@@ -10,10 +43,19 @@ router.use(function (req, res, next) {
 })
 
 router.use('/', async (req, res, next) => {
-  console.log(11111)
-  console.log(responseData)
-  res.json(responseData);
-  // next()
+  console.log(req.method)
+  console.log(req.path)
+  const method = req.method
+  const path = req.path
+  const doc = await Mock.findOne({
+    path,
+    type: { $regex: method, $options: '$i' } // 忽略大小写
+  })
+  if (doc && doc.reg) {
+    responseData.data = MockData(doc.reg)
+  }
+  res.json(responseData)
 })
 
-module.exports = router;
+
+module.exports = router
