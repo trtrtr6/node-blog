209d7f9cdc7e59ebc49c865814a6e2355083ddd3
trtrtr6 <2450260012@qq.com>
2020-09-14 17:07:04 +0800

    删除无用代码

--- build/models/Response.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = __importDefault(require("mongoose"));
-var responseSchema = require('../schemas/response');
-module.exports = mongoose_1.default.model('Response', responseSchema);
